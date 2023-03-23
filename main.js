c0cfaa8440ee12e2c20fdf62d5703a5f7b9d4b06
mkk4726 <mkk4726@naver.com>
2023-03-23 12:42:39 +0900

    Simplify code using router.
    그 전의 route들을 topic.js파일로 빼고
    /topic으로 시작하는 주소는 topicRouter로 처리하겠다고 등록하면 됨. 주의할 점은 '/topic'이 기본값으로 들어가서 topic.js에는 /topic지워줘야됨.

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ var sanitizeHtml = require('sanitize-html');
 var path = require('path');
 var qs = require('querystring');
 
+// make html source about list and body.
+const template = require('./libs/templates');
+
+// simplify code using router.
+var topicRouter = require('./routes/topic');
+
 // static file
 app.use(express.static('public')); // public directory안에서 파일을 찾겠다.
 // public아래에 있는 파일이나 디렉토리를 url을 통해 접근할 수 있게된다. -> 훨씬 더 안전해진다. 
@@ -34,8 +40,8 @@ app.get('*', (req, res, next) => {
   });
 });
 
-// make html source about list and body.
-const template = require('./libs/templates');
+// /topic으로 시작하는 주소들에게 topicRouter라는 middleware를 적용하겠다. 
+app.use('/topic', topicRouter);
 
 // route, routing
 // app.get('/', (req, res) => res.send('Hello World))
@@ -54,114 +60,7 @@ app.get('/', (req, res) => {
   res.status(200).send(html);
 });
 
-// implement create function
-// topic/create는 page가 아닌 생성page로 가기 위한 예약어로 사용하기 위해 위치를 위로 올렸다.
-app.get('/topic/create', (req, res) => {
-  var title = 'WEB - create';
-  var list = template.list(req.list);
-  // form tag를 이용해서 post request를 보낼 수 있다.
-  var html = template.HTML(title, list, ` 
-    <form action="/topic/create_process" method="post">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p>
-        <textarea name="description" placeholder="description"></textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
-    </form>
-  `, '');
-  res.status(200).send(html);
-});
 
-app.post('/topic/create_process', (req, res) => {
-  var post = req.body;
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
-    res.writeHead(302, {Location: `/topic/${title}`});
-    res.end();
-  })
-
-});
-
-// implement update function
-app.get('/topic/update/:pageId', (req, res) => {
-  var filteredId = path.parse(req.params.pageId).base;
-
-  fs.readFile(`./data/${filteredId}`, 'utf8', (err, desc) => {
-    var title = req.params.pageId;
-    var list = template.list(req.list);
-    // 위에서와 똑같이 form tag로 수정할 정보를 post request 보냄.
-    // input type="hidden"은 원래 id를 얻기 위한 코드로, 사용자가 수정할 수 없도록 숨겼다.
-    var html = template.HTML(title, list, `
-      <form action="/topic/update_process" method="post">
-        <input type="hidden" name="id" value="${title}">
-        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-        <p>
-          <textarea name="description" placeholder="description">${desc}</textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>`,
-    `<a href="/topic/create">create</a> <a href="/topic/update/id=${title}">update<a>`); // control
-
-    res.status(200).send(html); 
-  });
-});
-// 수정할 정보를 받아서 수정하는 코드
-app.post('/topic/update_process', (req, res) => {
-  var post = req.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, (err) => {
-    fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
-      res.writeHead(302, {Location: `/topic/${title}`}); // redirection
-      res.end();
-    });
-  });
-});
-
-// implement delete function
-// post request로 id값만 전달됨.
-app.post('/topic/delete_process', (req, res) => {
-  var post = req.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, (err) => {
-    res.redirect('/'); // 처음 페이지로 redirection
-  });
-});
-
-// list에 없는 값을 url로 요청했을 때 error을 발생시키자.
-app.get('/topic/:pageId', (req, res, next) => {
-  // for security. prevent input like "../password.js"
-  var filteredId = path.parse(req.params.pageId).base;
-  // read description from ./data/id
-  fs.readFile(`./data/${filteredId}`, 'utf8', (err, desc) => {
-    if (err) {
-      next(err); // error가 발생하면 다음 middleware을 호출하도록. err을 던진다. 
-    } else {
-      let title = req.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(desc, {
-        allowedTags:['h1']
-      });
-      let list = template.list(req.list);
-      let html = template.HTML(title, list , sanitizedDescription,
-        `<a href="/topic/create">create</a>
-        <a href="/topic/update/${sanitizedTitle}">update</a>
-        <form action="/topic/delete_process" method="post">
-          <input type="hidden" name="id" value="${sanitizedTitle}">
-          <input type="submit" value="delete">
-        </form>`
-      );
-      res.status(200).send(html);
-    }
-  });
-});
 
 // handle 404 response
 app.use((req, res, next) => {
